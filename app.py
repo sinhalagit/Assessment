f0a470f246314462b828e97876b64685e9601474
sinhalagit <167724667+sinhalagit@users.noreply.github.com>
2024-05-22 09:17:25 +1200

    space 4

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def print_all_artists():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all the results
-    print(f"First name        last name      stage name          genre         debut year     country")
+    print(f"First name        last name         stage name          genre         debut year     country")
     for artist in results:
         print(f"{artist[1]:<20} {artist[2]:<20} {artist[3]:<8} {artist[4]:<10} {artist[5]:<16} {artist[6]:<16}")
 
